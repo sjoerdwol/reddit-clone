491c22c1244f6a963327c1ef84f50ebf4db84710
sjoerdwol <sjoerd@woltersonline.net>
2023-01-12 19:31:41 +0100

    improved styling for NavBar, Startpage and SearchResults

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,18 +3,20 @@ import { AiOutlineReddit } from 'react-icons/ai';
 export function NavBar() {
 
   return (
-    <header className='flex flex-col gap-3 py-5 items-center shadow-white lg:gap-5 lg:py-7 2xl:gap-7 2xl:py-9'>
+    <header className='flex flex-col gap-3 py-5 items-center lg:flex-row lg:justify-around lg:gap-0 lg:py-7 2xl:gap-7 2xl:py-9'>
       <div className='flex items-center gap-2'>
-        <AiOutlineReddit className='text-2xl text-green-600 lg:text-4xl 2xl:text-6xl' />
-        <div className='font-bold lg:text-2xl 2xl:text-4xl'>
+        <AiOutlineReddit className='text-2xl text-green-600 md:text-3xl lg:text-4xl 2xl:text-6xl' />
+        <div className='font-bold md:text-xl lg:text-2xl 2xl:text-4xl'>
           <span className='text-green-600'>Reddit</span>Clone
         </div>
       </div>
       <nav>
-        <ul className='flex gap-5 font-semibold lg:text-xl 2xl:text-3xl'>
+        <ul className='flex gap-5 font-semibold md:text-lg lg:gap-7 lg:text-xl 2xl:text-3xl'>
           <li>Popular</li>
           <li>Gaming</li>
           <li>Sports</li>
+          <li className='hidden md:block'>Television</li>
+          <li className='hidden md:block'>Celebrity</li>
         </ul>
       </nav>
     </header>
--- src/features/SearchResults.jsx
+++ src/features/SearchResults.jsx
@@ -2,7 +2,7 @@ import { ResultCard } from "../components/ResultCard"
 
 export function SearchResults() {
   return (
-    <div className='flex flex-col md:flex-row md:flex-wrap md:justify-center'>
+    <div className='flex flex-col md:flex-row md:flex-wrap md:justify-center xl:px-5'>
       <ResultCard />
       <ResultCard />
       <ResultCard />
