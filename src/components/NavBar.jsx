ef72ba7a7c6556ec92d0fcdc4c16fdbad2499808
sjoerdwol <sjoerd@woltersonline.net>
2023-01-13 09:54:16 +0100

    small ux improvement changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -19,18 +19,18 @@ export function NavBar() {
   return (
     <header className='flex flex-col gap-3 py-5 items-center lg:flex-row lg:justify-around lg:gap-0 lg:py-7 2xl:gap-7 2xl:py-9'>
       <div className='flex items-center gap-2'>
-        <AiOutlineReddit className='text-2xl text-green-600 md:text-3xl lg:text-4xl 2xl:text-6xl' onClick={handleReset} />
-        <div className='font-bold md:text-xl lg:text-2xl 2xl:text-4xl' onClick={handleReset} >
+        <AiOutlineReddit className='hover:cursor-pointer text-2xl text-green-600 md:text-3xl lg:text-4xl 2xl:text-6xl' onClick={handleReset} />
+        <div className='font-bold hover:cursor-pointer md:text-xl lg:text-2xl 2xl:text-4xl' onClick={handleReset} >
           <span className='text-green-600'>Reddit</span>Clone
         </div>
       </div>
       <nav>
         <ul className='flex gap-5 font-semibold md:text-lg lg:gap-7 lg:text-xl 2xl:text-3xl'>
-          <li onClick={handleSubredditCall}>Popular</li>
-          <li onClick={handleSubredditCall}>Gaming</li>
-          <li onClick={handleSubredditCall}>Sports</li>
-          <li className='hidden md:block' onClick={handleSubredditCall}>Television</li>
-          <li className='hidden md:block' onClick={handleSubredditCall}>Celebrity</li>
+          <li className='hover:cursor-pointer' onClick={handleSubredditCall}>Popular</li>
+          <li className='hover:cursor-pointer' onClick={handleSubredditCall}>Gaming</li>
+          <li className='hover:cursor-pointer' onClick={handleSubredditCall}>Sports</li>
+          <li className='hidden hover:cursor-pointer md:block' onClick={handleSubredditCall}>Television</li>
+          <li className='hidden hover:cursor-pointer md:block' onClick={handleSubredditCall}>Celebrity</li>
         </ul>
       </nav>
     </header>
