c79bdeb064698eaf9ec2114aed7eb5402ff153b3
sjoerdwol <sjoerd@woltersonline.net>
2023-01-13 07:52:01 +0100

    improved display of posts

--- src/components/ResultCard.jsx
+++ src/components/ResultCard.jsx
@@ -1,19 +1,22 @@
 import moment from "moment/moment"
 
 export function ResultCard(props) {
-  console.log(props);
+  let title = props.result.title;
+  if(title.length > 40) {
+    title = title.slice(0, 40) + '...';
+  }
 
   return (
     <div className='flex justify-center px-5 pt-7 last:pb-7 xl:pt-10'>
       <div className='bg-zinc-800 flex flex-col items-center h-72 rounded-3xl w-72 xl:h-80 xl:w-80'>
-        <div className='py-3 font-medium text-xl'>
-          {props.result.title}
+        <div className='p-3 font-medium text-xl'>
+          {title}
         </div>
         <div className='px-3'>
-          <img src={props.result.url} alt="Image of post" className='max-h-44 max-w-60 rounded-lg xl:max-h-52 xl:max-w-64' />
+          <img src={props.result.url} alt="Image of post" className='max-h-44 max-w-60 rounded-lg xl:max-h-48 xl:max-w-64' />
         </div>
-        <div className='flex gap-14 mt-auto mb-4 xl:gap-24'>
-          <span>{moment.unix(props.result.created_utc).fromNow()} hours ago</span>
+        <div className='flex gap-14 mt-auto mb-4 xl:gap-18'>
+          <span>{moment.unix(props.result.created_utc).fromNow()}</span>
           <span>{props.result.num_comments} comments</span>
         </div>
       </div>
